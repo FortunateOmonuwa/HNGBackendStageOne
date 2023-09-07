688f8b9a81fc06cdba954187257d0b046a0e2d52
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2023-09-07 23:12:28 +0100

    Add project files.

---
+++ Controllers/InformationController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNGBackendStageOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InformationController : ControllerBase
+    {
+        private readonly Information _info;
+
+        public InformationController(Information info)
+        {
+            _info = info;
+        }
+
+        [HttpGet]
+        public IActionResult GetInfo(string slackName, string track)
+        {
+            try
+            {
+                if (track != _info.Track && slackName != _info.Slack_Name)
+                {
+                    return BadRequest("Invalid track and / or Slack Name");
+                }
+               
+                
+
+                var response = new
+                {
+                    _info.Slack_Name,
+                    _info.Current_Day,
+                    _info.CurrentUTCTime,
+                    _info.Track,
+                    _info.Github_File_URL,
+                    _info.Github_Repo_URL,
+                    _info.Status_Code
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Information.cs
@@ -0,0 +1,13 @@
+﻿namespace HNGBackendStageOne
+{
+    public class Information
+    {
+        public string Slack_Name { get; set; } = "Fortunate Omonuwa";
+        public string Track { get; set; } = "Backend";
+        public DayOfWeek Current_Day { get; set; } = DateTime.UtcNow.DayOfWeek;
+        public DateTime CurrentUTCTime { get; set; } = DateTime.UtcNow;
+        public string Github_File_URL { get; set; } = "";
+        public string Github_Repo_URL { get; set; } = "";
+        public int Status_Code { get; set; } = 200;
+    }
+}
