6f191a4ee8ac6fd2a1f65a136df1544fdb8342a6
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2023-09-08 10:54:54 +0100

    test

--- Information.cs
+++ Information.cs
@@ -5,7 +5,7 @@ namespace HNGBackendStageOne
     public class Information
     {
         public string Slack_Name { get; set; } = "Fortunate_Omonuwa";
-        public string Track { get; set; } = "Backend";
+        public string Track { get; set; } = "backend";
         public string Current_Day { get; set; } = DateTime.UtcNow.DayOfWeek.ToString();
         public DateTime Current_UTC_Time { get; set; } = DateTime.UtcNow;
         public string Github_File_URL { get; set; } = "https://github.com/FortunateOmonuwa/HNGBackendStageOne/blob/main/build/HNGBackendStageOne.exe";
