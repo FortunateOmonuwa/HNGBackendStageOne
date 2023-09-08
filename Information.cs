3c4d14228fc487c0eeb5c3e4859525db012e15c2
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2023-09-08 08:46:26 +0100

    added url

--- Information.cs
+++ Information.cs
@@ -6,7 +6,7 @@
         public string Track { get; set; } = "Backend";
         public DayOfWeek Current_Day { get; set; } = DateTime.UtcNow.DayOfWeek;
         public DateTime CurrentUTCTime { get; set; } = DateTime.UtcNow;
-        public string Github_File_URL { get; set; } = "";
+        public string Github_File_URL { get; set; } = "https://github.com/FortunateOmonuwa/HNGBackendStageOne/blob/main/build/HNGBackendStageOne.exe";
         public string Github_Repo_URL { get; set; } = "https://github.com/FortunateOmonuwa/HNGBackendStageOne";
         public int Status_Code { get; set; } = 200;
     }
